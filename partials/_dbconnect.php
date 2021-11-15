eaf8d26547a474daef50e79403a73f266f979a8e
IIT-Bhupendra <bhupendra.singh.che19@iitbhu.ac.in>
2021-11-15 16:14:04 +0530

    heroku envi vars configured

--- partials/_dbconnect.php
+++ partials/_dbconnect.php
@@ -2,10 +2,10 @@
     $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
     $dotenv->safeLoad();
 
-    $server = $_ENV['SERVER_NAME'];
-    $username = $_ENV['USER_NAME'];
-    $password = $_ENV['USER_IDENTIFICATION'];
-    $database = $_ENV['DB_NAME'];
+    $server = getenv('SERVER_NAME');
+    $username = getenv('USER_NAME');
+    $password = getenv('USER_IDENTIFICATION');
+    $database = getenv('DB_NAME');
 
     $conn = mysqli_connect($server, $username, $password, $database);
 ?>
